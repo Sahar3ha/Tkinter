d6985f05de71aca8fc61863d0ca4e2038ded4373
Sahar3ha <saharsha.xtha.3@gmail.com>
2021-12-22 14:50:38 +0545

    tkinter

---
+++ Tkinter/registration.py
@@ -0,0 +1,18 @@
+#registration forum
+from tkinter import *
+
+root = Tk()
+root.geometry("600x400")
+
+a=Label(root, text="First name").place(x=30,y=50)
+e1=Entry(root).place(x=100,y=50)
+b=Label(root, text="Last name").place(x=30,y=90)
+e2=Entry(root).place(x=100,y=90)
+c=Label(root, text="Email").place(x=30,y=160)
+e3=Entry(root).place(x=100,y=160)
+d=Label(root, text="Password").place(x=30,y=190)
+e4=Entry(root).place(x=100,y=190)
+
+e=Button(root, text="Login").place(x=240,y=250)
+f=Button(root, text="Cancel").place(x=290,y=250)
+root.mainloop()
